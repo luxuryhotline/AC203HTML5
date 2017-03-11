4271dde5b3421086c9c7e509f6d9242f005db12a
luxuryhotline <0220120315@harrowschool.hk>
2017-03-11 12:14:27 +0800

    phaser

--- Phaser/app.js
+++ Phaser/app.js
@@ -3,37 +3,59 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create
 function preload() {
 	game.load.image('sky','assets/sky.png');
 	game.load.image('ground','assets/platform.png');
-	game.load.image('diamond','assets/star.png');
+	game.load.image('star','assets/star.png');
 	game.load.spritesheet('dude','assets/dude.png',32,48);
 	game.load.spritesheet('baddie','assets/baddie.png',32,32);
 }
 
 function create() {
-	// game.add.sprite(0,0,'platform');
+	//enable acrade game physics in the game
 	game.physics.startSystem(Phaser.Physics.ARCADE);
-	game.add.sprite(0,0,'sky');
-	platforms = game.add.group();
-	platforms.enablebody = true;
 
+	//create sky
+	game.add.sprite(0,0,'sky');
+
+	//add platforms as a group, that contains ground and the 2 ledges
+	platforms = game.add.group();
+	platforms.enableBody = true;
+
+	//create the ground inside the group
 	var ground = platforms.create(0,game.world.height-50,'ground');
 	ground.scale.setTo(2,2);
 	ground.body.immovable = true;
 
-	var ledge = platforms.create(400,400,'ground');
+	//add the 2 ledges
+	var ledge = platforms.create(-150,250,'ground');
 	ledge.body.immovable = true;
-	ledge = platforms.create(-150,250,'ground');
+	ledge = platforms.create(400,400,'ground');
 	ledge.body.immovable = true;
+
+	//add a player to the game
 	player = game.add.sprite (32,game.world.height -150,'dude');
+	//adding player animations
 	player.animations.add('left',[0,1,2,3],10,true);
 	player.animations.add('right',[5,6,7,8],10,true);
+	//run physics on the player
 	game.physics.arcade.enable(player);
 	player.body.bounce.y = 0.2;
 	player.body.gravity.y = 300;
 	player.body.collideWorldBounds = true;
+	cursors = game.input.keyboard.createCursorKeys();
+	if(cursors.left.isDown){
+		player.body.velocity.x =-150;
+		player.animations.play('left');
+	}
+	else if(cursors.right.isDown){
+		player.body.velocity.x  = 150;
+		player.animations.play('right');
+	}
 
 
 }
 
 function update() {
+	//collision between player and platforms
 	game.physics.arcade.collide(player,platforms);
+
+
 }
