292f3d4e4e8242a83fd5bb9b3a0b84a1ab6eaa70
luxuryhotline <0220120315@harrowschool.hk>
2017-03-11 11:57:39 +0800

    phaser

--- Phaser/app.js
+++ Phaser/app.js
@@ -17,6 +17,19 @@ function create() {
 
 	var ground = platforms.create(0,game.world.height-50,'ground');
 	ground.scale.setTo(2,2);
+	ground.body.immovable = true;
+
+	var ledge = platforms.create(400,400,'ground');
+	ledge.body.immovable = true;
+	ledge = platforms.create(-150,250,'ground');
+	ledge.body.immovable = true;
+	player = game.add.sprite (32,game.world.height -150,'dude');
+	player.animations.add('left',[0,1,2,3],10,true);
+	player.animations.add('right',[5,6,7,8],10,true);
+	game.physics.arcade.enable(player);
+	player.body.bounce.y = 0.2;
+	player.body.gravity.y = 300;
+	player.body.collideWorldBounds = true;
 
 
 }
