c2b810aab4646f77a7d79e42f64e1ed2940d917a
luxuryhotline <0220120315@harrowschool.hk>
2017-03-11 11:44:13 +0800

    phaser

--- Phaser/app.js
+++ Phaser/app.js
@@ -23,6 +23,9 @@ function create() {
 	ledge.body.immovable = true;
 	ledge = platforms.create(-150,250,'ground');
 	ledge.body.immovable = true;
+	player = game.add.sprite (32,game.world.height -150,'dude');
+	player.animations.add('left',[0,1,2,3],10,true);
+	player.animations.add('right',[5,6,7,8],10,true);
 
 
 }
