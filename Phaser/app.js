a972b62690df8274eeb2406d7bc61aac60d5fa96
luxuryhotline <0220120315@harrowschool.hk>
2017-03-11 11:10:36 +0800

    phaser

--- Phaser/app.js
+++ Phaser/app.js
@@ -14,7 +14,7 @@ function create() {
 	game.add.sprite(0,0,'sky');
 	platforms = game.add.group();
 	platforms.enablebody = true;
-	var ground = platforms.create(0,game.world.height-50);
+	var ground = platforms.create(0,game.world.height-50,platform);
 
 }
 
