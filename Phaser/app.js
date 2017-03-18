f9875d7f8787570902d3d60dffa8b471b22fe42a
luxuryhotline <0220120315@harrowschool.hk>
2017-03-18 12:07:09 +0800

    phaser

--- Phaser/app.js
+++ Phaser/app.js
@@ -82,9 +82,12 @@ function update() {
 		star.kill();
 		score +=10;
 		scoreText.text='score : ' +score;
-		var star = stars.create(Math.random()*750, 20,'star');
-		star.body.gravity.y = 200;
-		star.body.bounce.y = 0.6 + Math.random()*0.3;
+		for (var i =0;i<4;i++){
+			var star = stars.create(Math.random()*750, 20,'star');
+			star.body.gravity.y = 200;
+			star.body.bounce.y = 0.6 + Math.random()*0.3;
+		}
+
 
 	}
 
